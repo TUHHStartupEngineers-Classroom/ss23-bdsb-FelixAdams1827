13775f591dfbf4d30b5c5de7095c287d90bc32ff
FelixAdams1827 <131382803+FelixAdams1827@users.noreply.github.com>
2023-04-28 18:12:54 +0200

    V4

--- Eigener Ordner/Data Acquisition/Aufgabe1API.R
+++ Eigener Ordner/Data Acquisition/Aufgabe1API.R
@@ -1,31 +1,37 @@
 #Step 1 Packages laden
-    library(httr)
-    library(jsonlite)
-    library(tidyverse)
-    library(ggplot2)
 
-#Step 2 Anfrage stellen 
-  # Wetter API 
-    resp <- GET("https://api.open-meteo.com/v1/forecast?latitude=53.55&longitude=9.99&hourly=temperature_2m")
-    hourly_temp <- rawToChar(resp$content) %>% fromJSON() 
-    time <- hourly_temp[["hourly"]][1]
-    temp <- hourly_temp[["hourly"]][2]
+      library(httr)
+      library(jsonlite)
+      library(tidyverse)
+      library(ggplot2)
 
-#Step 3 Daten darstellen 
-data <- data.frame(time, temp) %>% head(n = 30)
-data %>% 
-  ggplot(aes(x = time, y = temperature_2m)) +
-  geom_col() +
-  labs(
-    title = "Hourly Temperature in Hamburg, Germany",
-  ) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+#Step 2 Anfrage stellen
+      
+      resp <- GET("https://api.open-meteo.com/v1/forecast?latitude=53.55&longitude=9.99&hourly=windspeed_10m")
 
+#Step 3 Daten für Windgeschwindigkeit auslesen
 
+      windspeed10m <- rawToChar(resp$content) %>% fromJSON()
+      
+      time <- windspeed10m[["hourly"]][1]
+      windspeed_10m <- windspeed10m[["hourly"]][2]
+      
+      Geschwindigkeitsdaten <- data.frame(time, windspeed_10m) %>% head(n = 40)
+      
+      
+#Step 4 Visualisierung
+      
+      Geschwindigkeitsdaten %>%
+        
+      ggplot(aes(x = time, y = windspeed_10m)) +
+      geom_col(fill = "#2DC6D6") + 
 
-
-
-
-
-
-
-
+      theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+      theme(axis.text.y = element_text(angle = 45, hjust = 1)) +
+        
+      labs(
+          title    = "Windgeschwindigkeiten in Hamburg",
+          subtitle = "Stündlich gemessen vom 28.04.2023 0:00 Uhr bis 29.04.2023 15:00 Uhr",
+          x = "Zeitpunkt der Messung",
+          y = "Windgeschwindigkeit in km/h"
+          )
